62b36c4b01ed80751308b7a037a50ce72127f125
Samir-Sattarov <samirsattarov2004@gmail.com>
2023-11-03 18:58:50 +0500

    bug fixes

--- lib/entity/trips_category_entity.dart
+++ lib/entity/trips_category_entity.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class TripsCategoryEntity {
   final String image;
   final Color color;
+
+
+
   TripsCategoryEntity({
     required this.color,
     required this.image,
