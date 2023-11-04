2a624037b5dd4ea3b4cdbc58a721f27b22430a7d
Durdona <durdonajurakulova98@gmail.com>
2023-11-04 21:03:13 +0500

    Creation of Login screens

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,9 @@
 class Assets {
+  static String TAva = 'assets/images/Avatarka.png';
+  static String tStanding = 'assets/images/Standing.png';
+  static String tTravel = 'assets/images/city_travel.png';
+  static String tPonchik = 'assets/images/magic_donut.png';
+
 
 
 }
