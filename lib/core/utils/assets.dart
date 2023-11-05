e2725f06bd727f713c7d512293aaed6069b41a40
Makhmudkhon Muniskhonov <little_7o7@MacBook-Pro-14-little7o7.local>
2023-11-06 01:09:30 +0500

    finish all

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,4 +2,6 @@ class Assets {
   static const String FavoritesSectionSecondScreenAvatar = "assets/images/FavoritesSectionSecondScreenAvatar.png";
   static const String FavoritesSectionFirstScreenAdvImage = "assets/images/FavoritesSectionFirstScreenAdvImage.png";
   static const String FavoritesSectionSecondScreenListCardImage = "assets/images/FavoritesSectionSecondScreenListCardImage.png";
+  static const String FavoritesSectionThirdScreenRecommendedCardPhoto = "assets/images/FavoritesSectionThirdScreenRecommendedCardPhoto.png";
+  static const String FavoritesSectionThirdScreenPopularCardPhoto = "assets/images/FavoritesSectionThirdScreenPopularCardPhoto.png";
 }
--- lib/screens/favorites_section_third_screen.dart
+++ lib/screens/favorites_section_third_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:student_travel_application/core/utils/assets.dart';
-
+import 'package:student_travel_application/widgets/favorites_section_third_screen_recommended_card_widget.dart';
+import 'package:student_travel_application/widgets/favorites_section_third_screen_popular_card_widget.dart';
 import 'package:student_travel_application/widgets/favorites_top_custom_icon_button.dart';
+import 'package:student_travel_application/entity/favorites_section_third_screen_recommended_card_entity.dart';
+import 'package:student_travel_application/entity/favorites_section_third_screen_popular_card_entity.dart';
 
 class FavoritesSectionThirdScreen extends StatefulWidget {
   static route() =>
@@ -16,6 +19,46 @@ class FavoritesSectionThirdScreen extends StatefulWidget {
 
 class _FavoritesSectionThirdScreenState
     extends State<FavoritesSectionThirdScreen> {
+  final FavoritesSectionThirdScreenRecommendedCardEntity recommendedCard =
+      FavoritesSectionThirdScreenRecommendedCardEntity(
+    title: 'Italy',
+    description: 'Discover and find new roomates around the world',
+    image: Assets.FavoritesSectionThirdScreenRecommendedCardPhoto,
+  );
+
+  final List<FavoritesSectionThirdScreenPopularCardEntity> popularCards = [
+    FavoritesSectionThirdScreenPopularCardEntity(
+      image: Assets.FavoritesSectionThirdScreenPopularCardPhoto,
+      title: 'John',
+      price: 500,
+    ),
+    FavoritesSectionThirdScreenPopularCardEntity(
+      image: Assets.FavoritesSectionThirdScreenPopularCardPhoto,
+      title: 'John',
+      price: 500,
+    ),
+    FavoritesSectionThirdScreenPopularCardEntity(
+      image: Assets.FavoritesSectionThirdScreenPopularCardPhoto,
+      title: 'John',
+      price: 500,
+    ),
+    FavoritesSectionThirdScreenPopularCardEntity(
+      image: Assets.FavoritesSectionThirdScreenPopularCardPhoto,
+      title: 'John',
+      price: 500,
+    ),
+    FavoritesSectionThirdScreenPopularCardEntity(
+      image: Assets.FavoritesSectionThirdScreenPopularCardPhoto,
+      title: 'John',
+      price: 500,
+    ),
+    FavoritesSectionThirdScreenPopularCardEntity(
+      image: Assets.FavoritesSectionThirdScreenPopularCardPhoto,
+      title: 'John',
+      price: 500,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,15 +79,65 @@ class _FavoritesSectionThirdScreenState
         actions: <Widget>[
           FavoritesTopCustomIconButton(
             onPressed: () {},
-            icon: Icons.star,
-            iconColor: const Color(0xff656F77),
+            icon: Icons.shopping_cart,
+            iconColor: Colors.black,
           ),
           const SizedBox(
             width: 8,
           )
         ],
       ),
-      body: Placeholder(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FavoritesSectionThirdScreenRecommendedCardWidget(
+                    entity: recommendedCard,
+                    onPressed: () {},
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Popular',
+                    style: GoogleFonts.nunito(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      height: 1.1,
+                      letterSpacing: -0.2,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 221,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: popularCards.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      FavoritesSectionThirdScreenPopularCardWidget(
+                        entity: popularCards[index],
+                        onPressed: () {},
+                      ),
+                      index == popularCards.length - 1
+                          ? const SizedBox(width: 16)
+                          : Container(),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
