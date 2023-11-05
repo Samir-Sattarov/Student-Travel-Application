922aed80ebfef973e804f6ba5ebf37d68654b5ad
timurIsakov <timur092000@gmail.com>
2023-11-05 11:24:21 +0500

    Added 2 screen

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,17 @@
 class Assets {
+  //png
+  static const String tCampingImage = "assets/images/camping_image.png";
+  static const String tCityOfLondon = "assets/images/city_of_london_image.png";
+  static const String tAvatarGirlIcon = "assets/images/avatar_girl_icon.png";
+  //svg
+  static const String tLocationIcon = "assets/svg/location_icon.svg";
+  static const String tLikeIcon = "assets/svg/like_icon.svg";
+  static const String tSendIcon = "assets/svg/send_icon.svg";
+  static const String tMoreVerticalIcon = "assets/svg/more_vertical_icon.svg";
+  static const String tMuseumIcon = "assets/svg/museum_icon.svg";
+  static const String tPhotoIcon = "assets/svg/photo_icon.svg";
+  static const String tFaceIcon = "assets/svg/face_icon.svg";
+
 
 
 }
